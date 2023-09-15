64451ad41d178b84f017581082984cc0af4a6c3e
XomakDevelopment <145150850+XomakDevelopment@users.noreply.github.com>
2023-09-15 23:56:41 +0300

    Update KeySystem ScriptHub Tutorial.lua

--- ping_client.lua
+++ ping_client.lua
@@ -1,29 +1,19 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local plr = game.Players.LocalPlayer
-local Window = OrionLib:MakeWindow({Name = "PING CLIENT | (v1.2)", HidePremium = false, SaveConfig = true, ConfigFolder = "pingclient", IntroEnabled = true, IntroText = "PING CLIENT | (v1.2)"})
+local Window = OrionLib:MakeWindow({Name = "NameHub)", HidePremium = false, SaveConfig = true, ConfigFolder = "nameCFGfolder", IntroEnabled = true, IntroText = "INTRO TEXT - NAME)"})
 
 OrionLib:MakeNotification({
-	Name = "PING CLIENT | (v1.2)!",
+	Name = "name hub",
 	Content = "You are logged in as "..plr.name.."!",
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
 
-_G.Key = "pingclient_1_2"
+_G.Key = "password"
 _G.KeyInput = "string"
 
 function MakeScriptsHub()
-	local  Hub = Window:MakeTab({
-	Name = "Hub",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-	})
-	OrionLib:MakeNotification({
-	Name = "Hub",
-	Content = "ScriptHub is Activated!",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-	})
+	print("LOL")
 end
 
 function CorrectkeyNotification()
@@ -59,33 +49,11 @@ Tab:AddTextbox({
 	end	  
 })
 
-function hub1()
-	Hub:AddButton({
-	Name = "Button!",
-	Callback = function()
-      		print("1")
-  	end
-	})
-end
-
 Tab:AddButton({
 	Name = "CheckKey",
 	Callback = function()
       		if _G.KeyInput == _G.Key then
 				MakeScriptsHub()
-				CorrectkeyNotification()
-				local  Hub = Window:MakeTab({
-				Name = "Hub",
-				Icon = "rbxassetid://4483345998",
-				PremiumOnly = false
-				})
-				OrionLib:MakeNotification({
-				Name = "Hub",
-				Content = "ScriptHub is Activated!",
-				Image = "rbxassetid://4483345998",
-				Time = 5
-				})
-				hub1()
 			else
 				IncorrectkeyNotification()
 		end
