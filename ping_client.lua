3c8c00828ed0dfec414c2925fe8245d8200c354a
XomakDevelopment <145150850+XomakDevelopment@users.noreply.github.com>
2023-09-15 23:35:19 +0300

    Update ping_client.lua

--- ping_client.lua
+++ ping_client.lua
@@ -12,6 +12,38 @@ OrionLib:MakeNotification({
 _G.Key = "pingclient_1_2"
 _G.KeyInput = "string"
 
+function MakeScriptsHub()
+	local  Hub = Window:MakeTab({
+	Name = "Hub",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+	})
+	OrionLib:MakeNotification({
+	Name = "Hub",
+	Content = "ScriptHub is Activated!",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+	})
+end
+
+function CorrectkeyNotification()
+	OrionLib:MakeNotification({
+	Name = "Correct key!",
+	Content = "You have entered Correct Key! ",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+	})
+end
+
+function IncorrectkeyNotification()
+	OrionLib:MakeNotification({
+	Name = "Incorrect key!",
+	Content = "You have entered Incorrect Key! ",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+	})
+end
+
 local Tab = Window:MakeTab({
 	Name = "Key",
 	Icon = "rbxassetid://4483345998",
@@ -25,4 +57,37 @@ Tab:AddTextbox({
 	Callback = function(Value)
 		_G.KeyInput = Value
 	end	  
+})
+
+function hub1()
+	Hub:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("1")
+  	end
+	})
+end
+
+Tab:AddButton({
+	Name = "CheckKey",
+	Callback = function()
+      		if _G.KeyInput == _G.Key then
+				MakeScriptsHub()
+				CorrectkeyNotification()
+				local  Hub = Window:MakeTab({
+				Name = "Hub",
+				Icon = "rbxassetid://4483345998",
+				PremiumOnly = false
+				})
+				OrionLib:MakeNotification({
+				Name = "Hub",
+				Content = "ScriptHub is Activated!",
+				Image = "rbxassetid://4483345998",
+				Time = 5
+				})
+				hub1()
+			else
+				IncorrectkeyNotification()
+		end
+  	end
 })
